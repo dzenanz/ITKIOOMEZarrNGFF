48c07d05d6daad19efeb61a19fa1415d6df1753f
Dženan Zukić <dzenan.zukic@kitware.com>
2022-01-28 17:44:56 -0500

    WIP: This works. It creates v3 file.

--- test/itkOMEZarrNGFFImageIOTest.cxx
+++ test/itkOMEZarrNGFFImageIOTest.cxx
@@ -28,6 +28,7 @@
 #include "xtensor-io/xio_binary.hpp"
 
 #include "xtensor-io/xio_blosc.hpp"
+#include "xtensor-io/xio_gzip.hpp"
 #include "xtensor-zarr/xzarr_compressor.hpp"
 
 #define SPECIFIC_IMAGEIO_MODULE_TEST
@@ -36,50 +37,59 @@
 int
 itkOMEZarrNGFFImageIOTest(int argc, char * argv[])
 {
-  //xt::xzarr_register_compressor<xt::xzarr_file_system_store, xt::xio_binary_config>();
-  //xt::xzarr_register_compressor<xt::xzarr_file_system_store, xt::xio_blosc_config>();
+  // xt::xzarr_register_compressor<xt::xzarr_file_system_store, xt::xio_binary_config>();
+  // xt::xzarr_register_compressor<xt::xzarr_file_system_store, xt::xio_gzip_config>();
+  xt::xzarr_register_compressor<xt::xzarr_file_system_store, xt::xio_blosc_config>();
 
   {
-    // create a hierarchy on the local file system
+    // open a hierarchy
     xt::xzarr_file_system_store store("test.zr3");
-    auto h = xt::get_zarr_hierarchy(store);
-
+    auto                        h = xt::create_zarr_hierarchy(store);
     // create an array in the hierarchy
-    nlohmann::json attrs = { {"question", "life"}, {"answer", 42} };
-    using S = std::vector<std::size_t>;
-    S shape = { 4, 4 };
-    S chunk_shape = { 2, 2 };
+    nlohmann::json      attrs = { { "question", "life" }, { "answer", 42 } };
+    std::vector<size_t> shape = { 4, 4 };
+    std::vector<size_t> chunk_shape = { 2, 2 };
+    // specify options
+    xt::xzarr_create_array_options<xt::xio_blosc_config> o;
+    o.chunk_memory_layout = 'C';
+    o.chunk_separator = '/';
+    o.attrs = attrs;
+    o.chunk_pool_size = 2;
+    o.fill_value = 1.2;
 
-    xt::zarray a = h.create_array(
-      "/arthur/dent",  // path to the array in the store
-      shape,  // array shape
-      chunk_shape,  // chunk shape
-      "<f8"  // data type, here little-endian 64-bit floating point
+    xt::zarray z = h.create_array("/arthur/dent", // path to the array in the store
+                                  shape,          // array shape
+                                  chunk_shape,    // chunk shape
+                                  "<f8",          // data type, here little-endian 64-bit floating point
+                                  o               // options
     );
 
-    //// write array data
-    //a(2, 1) = 3.;
+    // write array data
+    auto a = z.get_array<double>();
+    a(2, 1) = 3.;
 
-    //// read array data
-    //std::cout << a(2, 1) << std::endl;
-    //// prints `3.`
-    //std::cout << a(2, 2) << std::endl;
-    //// prints `0.` (fill value)
-    //std::cout << a.attrs() << std::endl;
-    //// prints `{"answer":42,"question":"life"}`
+    // read array data
+    std::cout << a(2, 1) << std::endl;
+    // prints `3.`
+    std::cout << a(2, 2) << std::endl;
+    // prints `1.2` (fill value)
+
+    // the below properly works only with version 3 of the format
 
     // create a group
     auto g = h.create_group("/tricia/mcmillan", attrs);
+    std::cout << g.attrs() << std::endl;
+    // prints `{"answer":42,"question":"life"}`
 
     // explore the hierarchy
     // get children at a point in the hierarchy
     std::string children = h.get_children("/").dump();
     std::cout << children << std::endl;
-    // prints `{"arthur":"implicit_group","marvin":"explicit_group","tricia":"implicit_group"}`
+    // prints `{"arthur":"implicit_group","tricia":"implicit_group"}`
     // view the whole hierarchy
     std::string nodes = h.get_nodes().dump();
     std::cout << nodes << std::endl;
-    // prints `{"arthur":"implicit_group","arthur/dent":"array","tricia":"implicit_group","tricia/mcmillan":"explicit_group"}`
+    // prints `{"arthur\\dent":"array","tricia\\mcmillan":"explicit_group"}`
   }
 
   if (argc < 3)
